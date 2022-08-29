9e0bf31481fd8a9df69a33da29d8e34ab56c02c7
PippinBo <zboo0001@student.monash.edu>
2022-08-29 22:05:19 +1000

    delete weather api

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,14 +1,14 @@
-import axios from "axios";
-
-const URL = "https://openweathermap.org/data/2.5/weather";
-const API_key = "88770383aedf0d42f7cc2fd54baa356b";
-export const fetchWeather = async () => {
-    const {data} = await axios.get(URL, {
-        params: {
-            q: query,
-            units: 'metric',
-            APPID: API_key,
-        }
-    });
-    return data;
-}
+// import axios from "axios";
+//
+// const URL = "https://openweathermap.org/data/2.5/weather";
+// const API_key = "88770383aedf0d42f7cc2fd54baa356b";
+// export const fetchWeather = async () => {
+//     const {data} = await axios.get(URL, {
+//         params: {
+//             q: query,
+//             units: 'metric',
+//             APPID: API_key,
+//         }
+//     });
+//     return data;
+// }
